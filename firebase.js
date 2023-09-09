9d7d1614e6a0090b5c74a69ffa6ccc1da21099cc
Emilee <emilee.mapanao@gmail.com>
2023-09-08 17:22:36 -0700

    anon button, past comments and timestamp working

--- firebase.js
+++ firebase.js
@@ -1,75 +1,64 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDk1UgfIbXomLDJqi473t73dqC9YaPuSHQ",
-    authDomain: "memorialwebsite-82edf.firebaseapp.com",
-    databaseURL: "https://memorialwebsite-82edf-default-rtdb.firebaseio.com",
-    projectId: "memorialwebsite-82edf",
-    storageBucket: "memorialwebsite-82edf.appspot.com",
-    messagingSenderId: "1029058327991",
-    appId: "1:1029058327991:web:411d72a5494979eae8a432",
-    measurementId: "G-7N1DDTDBLV"
-  };
+  apiKey: "AIzaSyDk1UgfIbXomLDJqi473t73dqC9YaPuSHQ",
+  authDomain: "memorialwebsite-82edf.firebaseapp.com",
+  databaseURL: "https://memorialwebsite-82edf-default-rtdb.firebaseio.com",
+  projectId: "memorialwebsite-82edf",
+  storageBucket: "memorialwebsite-82edf.appspot.com",
+  messagingSenderId: "1029058327991",
+  appId: "1:1029058327991:web:411d72a5494979eae8a432",
+  measurementId: "G-7N1DDTDBLV"
+};
 
-    // Initialize Firebase
-   firebase.initializeApp(firebaseConfig);
+// Add anon button
+// order comments from newest to oldest
 
-   //reference your database
-   var db = firebase.database()
-   const ref = db.ref("messages")
 
-   //function save() {
-    //var name = document.getElementById('tbName').value;
-    //var comment = document.getElementById('txComment').value;
-    //var newComment = document.getElementById('newcomment').value;
-    //reads comment, if empty asks user to fill block
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-      //db.ref('users/').push({
-       // name : name,
-      //  comment : comment,
-      //  newComment : newComment
-    //})
-    //alert('Saved')
-  
-    
-    // if user enters name, no push to database
-    // if user does not enter name, pushses anon to db
-   //}
+//reference your database
+var db = firebase.database()
+const ref = db.ref("messages")
 
-  // function get() {
-  //  var username = document.getElementById('tbName').value
-   // var user_ref = db.ref('users/' + tbName)
-  //  user_ref.on('value', function(snapshot){
-  //    var data = snapshot.val()
-
-  //    console.log(data)
-   // })
-//  }
-   
-  //function update() {
-  //  var comment = document.getElementById('txComment').value
-  //  var updates = {
-  //    comment : comment
-//
- //   }
- // }
-
- function save(){
+function save() {
+  console.log('save was clicked');
   const form = document.getElementById('newcomment');
 
-  form.addEventListener("submit",(e)=>{
+  form.addEventListener("submit", (e) => {
+    if (document.getElementById('anon').checked){
+      name = 'anonymous'
+    }
+    else {
     e.preventDefault();
-    const name = document.getElementById('tbName').value
+    var name = document.getElementById('tbName').value
+    }
     const text = document.getElementById('txComment').value
 
     ref.push({
-      name : name,
+      name: name,
       text: text,
+      when: firebase.database.ServerValue.TIMESTAMP
 
     })
 
     alert('Saved')
     console.log(name, text)
-    form.reset
+    form.reset()
   })
 }
 
-   
+function showPastComments() {
+  console.log('yes showPastComments() is running')
+  var showat = document.getElementById('pastcomments');
+  var commentsRef = firebase.database().ref('messages/');
+  commentsRef.once('value', function (snapshot) {
+    snapshot.forEach(function (itemSnapshot) {
+      var itemData = itemSnapshot.val();
+      var comment = itemData.text;
+      var name = itemData.name;
+      var when = new Date(itemData.when).toLocaleDateString("en-us");
+      showat.innerHTML += "<li>" + comment + "<span> --" + name + " (" + when + ") </span></li>";
+    })
+  })
+}
+showPastComments()
