863b49adb7d49a1aeaeba9f750b048181f46f2da
Emilee <emilee.mapanao@gmail.com>
2023-09-10 21:37:12 -0700

    Updated Database

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDk1UgfIbXomLDJqi473t73dqC9YaPuSHQ",
-  authDomain: "memorialwebsite-82edf.firebaseapp.com",
-  databaseURL: "https://memorialwebsite-82edf-default-rtdb.firebaseio.com",
-  projectId: "memorialwebsite-82edf",
-  storageBucket: "memorialwebsite-82edf.appspot.com",
-  messagingSenderId: "1029058327991",
-  appId: "1:1029058327991:web:411d72a5494979eae8a432",
-  measurementId: "G-7N1DDTDBLV"
+  apiKey: "AIzaSyBAP2HgXx-voygp6a1bLk_J0GiW7Q1TcEM",
+  authDomain: "brandonswebsite-93312.firebaseapp.com",
+  databaseURL: "https://brandonswebsite-93312-default-rtdb.firebaseio.com",
+  projectId: "brandonswebsite-93312",
+  storageBucket: "brandonswebsite-93312.appspot.com",
+  messagingSenderId: "16043005500",
+  appId: "1:16043005500:web:0e8ff885d5ec884e5f1539",
+  measurementId: "G-C8WKGPL5Y6"
 };
 
 // Initialize Firebase
