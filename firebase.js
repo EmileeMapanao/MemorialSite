f38ca7090bc49cdb2a8323e3a21b5dd93ac4887f
BaptizedLion <michael.mapanao@gmail.com>
2023-09-16 20:43:25 -0700

    mobile fix

--- firebase.js
+++ firebase.js
@@ -14,81 +14,85 @@ const firebaseConfig = {
   storageBucket: "brandonswebsite-93312.appspot.com",
   messagingSenderId: "16043005500",
   appId: "1:16043005500:web:0e8ff885d5ec884e5f1539",
-  measurementId: "G-C8WKGPL5Y6"
+  measurementId: "G-C8WKGPL5Y6",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 //reference your database
-var db = firebase.database()
-const ref = db.ref("messages")
+var db = firebase.database();
+const ref = db.ref("messages");
 
 // default to name being required
-document.getElementById('tbName').required = true;
+document.getElementById("tbName").required = true;
 
 // Disabled name tb if anonymous checkbox is ticked
 function checkAnon() {
-  if (document.getElementById('anon').checked == false) {
+  if (document.getElementById("anon").checked == false) {
     document.getElementById("tbName").disabled = false;
-  }
-  else {
-    document.getElementById('tbName').required = false;
+  } else {
+    document.getElementById("tbName").required = false;
     document.getElementById("tbName").disabled = true;
   }
 }
 
-
 // Submit form and push to server
 function save() {
-  const form = document.getElementById('newcomment');
+  const form = document.getElementById("newcomment");
   form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const text = document.getElementById('txComment').value  // get comment
-    if (document.getElementById('anon').checked == false) {
-      var name = document.getElementById('tbName').value  // get name if not anon
-    }
-    else {
-      name = 'Anonymous'
+    e.preventDefault();
+    const text = document.getElementById("txComment").value; // get comment
+    if (document.getElementById("anon").checked == false) {
+      var name = document.getElementById("tbName").value; // get name if not anon
+    } else {
+      name = "Anonymous";
     }
     if (name == "" && message == "") {
       // ignore double form submission
-    }
-    else {
+    } else {
       ref.push({
         name: name,
         text: text,
-        when: firebase.database.ServerValue.TIMESTAMP
-      })
+        when: firebase.database.ServerValue.TIMESTAMP,
+      });
       //alters button when form submitted
-      changeButton()
+      changeButton();
     }
-    form.reset()
-    window.location.reload()
+    form.reset();
+    window.location.reload();
     form.reset();
     window.location.reload(); // auto refresh to display new comment
-  })
+  });
 }
 
 // Pull data from server and display on page
 function showPastComments() {
-  var showat = document.getElementById('pastcomments');
-  var commentsRef = firebase.database().ref('messages/');
-  commentsRef.once('value', function (snapshot) {
+  var showat = document.getElementById("pastcomments");
+  var commentsRef = firebase.database().ref("messages/");
+  commentsRef.once("value", function (snapshot) {
     var com = snapshot.forEach(function (itemSnapshot) {
       var itemData = itemSnapshot.val();
       var comment = itemData.text;
       var name = itemData.name;
       var when = new Date(itemData.when).toLocaleDateString("en-us");
-      showat.innerHTML += "<li>" + comment + "<br><br>" + "<span> <strong>" + name + "</strong><br>" + when + "</span></li>";
-    })
-  })
+      showat.innerHTML +=
+        "<li>" +
+        comment +
+        "<br><br>" +
+        "<span> <strong>" +
+        name +
+        "</strong><br>" +
+        when +
+        "</span></li>";
+    });
+  });
 }
-showPastComments()
+showPastComments();
 
 //button change
 function changeButton() {
   var btn = document.getElementById("btn");
-  btn.value = 'my value'; // will just add a hidden value
-  btn.innerHTML = 'Submitted!';
+  btn.value = "my value"; // will just add a hidden value
+  btn.innerHTML = "Submitted!";
 }
